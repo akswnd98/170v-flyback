778b3760c6a26cff493e35e2fca93a03649f4555
akswnd98 <akswnd98@gmail.com>
2025-10-11 19:26:24 +0900

    first

---
+++ design.py
@@ -0,0 +1,49 @@
+import matplotlib.pyplot as plt
+import control as co
+import numpy as np
+
+V_IN, V_OUT = 5, 170
+N_PS = 1 / 10
+D = N_PS * V_OUT / (V_IN + N_PS * V_OUT)
+D_PRIME = 1 - D
+L = 10e-6
+R_LOAD, C_OUT = 17e+3, 4.7e-6
+V_REF = 1.
+f_SW = 350e+3
+T_SW = 1 / f_SW
+R_FB1 = 330e+3
+R_FB2 = R_FB1 * V_REF / (V_OUT - V_REF)
+
+R_s = 0.01
+V_SL = 40e-3
+S_n, S_f, S_e = V_IN / L, N_PS * V_OUT / L, V_SL * f_SW / R_s
+
+g_m = 2e-3
+R_ea_o = 10e+6
+G_comp = 0.142
+A_ea_vol = g_m * R_ea_o
+f_p2, f_z3 = 80, 4000
+
+R_c1 = f_p2 / (f_z3 - f_p2) * R_ea_o
+C_c1 = 1 / (R_c1 * 2 * np.pi * f_z3)
+A_ea = A_ea_vol * G_comp
+
+F_m = 1 / (R_s * (S_n + S_e) * T_SW)
+G_di = co.tf([(S_n + S_f)], [1, 0])
+H_e = co.tf([1 / ((np.pi * f_SW) ** 2), -1 / (2 * f_SW), 1], [1])
+
+G_ivo = N_PS * co.tf([-V_OUT * L / (V_IN * R_LOAD * D_PRIME * N_PS), 1], [C_OUT / D_PRIME, 1 / (D_PRIME * R_LOAD) + N_PS * V_OUT / (V_IN * R_LOAD)])
+
+G_div = R_FB2 / (R_FB1 + R_FB2)
+
+G_fb = A_ea * co.tf([1 / (2 * np.pi * f_z3), 1], [1 / (2 * np.pi * f_p2), 1])
+
+G_inner_loop = co.feedback(F_m * G_di, R_s * H_e)
+
+G_overall_loop = G_inner_loop * G_ivo * G_div * G_fb
+
+co.bode(G_overall_loop, np.logspace(0, 7, 1000), Hz=True, color='green')
+co.bode(G_inner_loop * G_ivo * G_div, np.logspace(0, 7, 1000), Hz=True, color='blue')
+co.bode(G_fb, np.logspace(0, 7, 1000), Hz=True, color='red')
+
+plt.show()
